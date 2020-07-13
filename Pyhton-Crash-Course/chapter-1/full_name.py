55378e87b671661c215a1fa674cc1df640fa1238
Josh Miller <jmiller52.ca@gmail.com>
2020-07-13 09:16:34 -0600

    more simple variable practice

---
+++ Pyhton-Crash-Course/chapter-1/full_name.py
@@ -0,0 +1,9 @@
+first_name = "josh"
+last_name = "miller"
+full_name = f"{first_name} {last_name}"
+print(full_name)
+
+print(f"Hello, {full_name.title()}!")
+
+message = f"Hello, {full_name.title()}!"
+print(message)
---
+++ Pyhton-Crash-Course/chapter-1/name.py
@@ -0,0 +1,4 @@
+name = 'josh miller'
+print(name.title())
+print(name.upper())
+print(name.lower())
