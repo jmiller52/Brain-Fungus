e32b7e56c215a32c6a69d0e7b71f87889cc5eaa1
Josh Miller <jmiller52.ca@gmail.com>
2020-07-13 08:29:28 -0600

    First up to content in the Python Crash Course

---
+++ Pyhton-Crash-Course/chapter-1/simple_message.py
@@ -0,0 +1,2 @@
+only_message = "This will be the only message printed in this python file"
+print(only_message)
---
+++ Pyhton-Crash-Course/chapter-1/simple_messages.py
@@ -0,0 +1,5 @@
+single_message = "This is a single message assigned to a variable that is going to be printed"
+print(single_message)
+
+single_message = "I decided to change the message being printed to this :)"
+print(single_message)
